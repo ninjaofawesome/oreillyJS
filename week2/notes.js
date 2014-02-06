c95e2b6fa3155eb6f320213a810d2745403c75bd
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-02-06 13:37:10 -0500

    started chapter two

---
+++ week2/notes.js
@@ -0,0 +1,12 @@
+//EXAMPLE OF LEXICAL STRUCTURE
+
+//Ruby Method
+def my_method
+	print "this is a method"
+end
+
+//Javascript function
+var = myFunction(awesome){
+	return("javascript is " + awesome)
+}
+

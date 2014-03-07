0bef5afcc78e945ca86310c0f45e039119259793
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-03-07 15:11:17 -0500

    bitwise operators!  Like whoa

---
+++ week4/bitwise.js
@@ -0,0 +1,55 @@
+//Bitwise Numeric Representation Examples
+
+10 === 1010
+5 === 101
+
+
+
+console.log (2 ^ 3);
+// 2 === 10 and 3 === 11
+// result => 1
+
+console.log (1 ^ 0);
+// 1 === 1 and 0 === 0
+// result => 1
+
+console.log (7 ^ 3);
+// 7 === 111 and 3 === 11
+// result => 4, which in binary is 100
+
+
+console.log(~4);
+console.log(-4-1);
+//both equal -5.
+
+console.log(~0);
+console.log(0-1);
+//both equal -1.
+
+
+console.log (10 << 1);
+// 20
+
+console.log (3 << 1);
+//6
+
+console.log (3 << 2);
+//12
+
+console.log (1 >> 1);
+//0
+
+console.log (12 >> 1);
+//6
+
+
+
+
+
+
+
+
+
+
+
+
